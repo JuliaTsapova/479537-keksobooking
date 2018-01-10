faf7ce3e4ce42d45792a17eb9313ae14388d098b
Julia Tsapova <12tsapova@gmail.com>
2018-01-10 21:49:28 +0700

    Личный проект: шэр, лайк(2)

--- js/avatar.js
+++ js/avatar.js
@@ -5,22 +5,41 @@
 
   var fileChooser = document.querySelector('#avatar');
   var preview = document.querySelector('.notice__preview > img');
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
+  var dropZone = document.querySelector('label[for=avatar]');
 
+  var addAvatar = function (file) {
+    var fileName = file.name.toLowerCase();
     var matches = FILE_TYPES.some(function (it) {
       return fileName.endsWith(it);
     });
 
     if (matches) {
       var reader = new FileReader();
-
       reader.addEventListener('load', function () {
         preview.src = reader.result;
       });
-
       reader.readAsDataURL(file);
     }
+  };
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    addAvatar(file);
   });
+
+  dropZone.addEventListener('dragover', function (evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = 'copy';
+    },
+    false);
+
+  dropZone.addEventListener('drop', function (evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      var files = evt.dataTransfer.files;
+      var file = files[0];
+      addAvatar(file);
+    },
+    false);
 })();
--- js/housing-photo.js
+++ js/housing-photo.js
@@ -5,17 +5,19 @@
 
   var fileChooser = document.querySelector('#images');
   var photoBox = document.querySelector('.form__photo-container');
+  var photos = document.querySelectorAll('.loaded-image');
+  var draggedItem = null;
+  var dropZone = document.querySelector('label[for=images]');
+  var draggedItem = null;
 
-  var addElement = function (newElement, element) {
+  var createElement = function (newElement, element) {
     var newElem = document.createElement(newElement);
     element.appendChild(newElem);
     return newElem;
   };
 
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
+  var addElement = function (file) {
     var fileName = file.name.toLowerCase();
-
     var matches = FILE_TYPES.some(function (it) {
       return fileName.endsWith(it);
     });
@@ -23,11 +25,56 @@
     if (matches) {
       var reader = new FileReader();
       reader.addEventListener('load', function () {
-        var newImage = addElement('img', photoBox);
+        var newImage = createElement('img', photoBox);
         newImage.src = reader.result;
         newImage.classList.add('loaded-image');
+        newImage.draggable = 'true';
+        return newImage;
       });
       reader.readAsDataURL(file);
     }
+  };
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    addElement(file);
   });
+
+  console.log(photos);
+
+  dropZone.addEventListener('dragover', function (evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = 'copy';
+    },
+    false);
+
+  dropZone.addEventListener('drop', function (evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      var files = evt.dataTransfer.files;
+      var file = files[0];
+      addElement(file);
+    },
+    false);
+
+  for (var i = 0; i < photos.length; i++) {
+
+    photos[i].addEventListener('dragstart', function (evt) {
+      if (evt.target.tagName.toLowerCase() === 'img') {
+        draggedItem = evt.target;
+        evt.dataTransfer.setData('text/plain', evt.target.alt);
+      }
+    });
+
+    photos[i].addEventListener('dragover', function (evt) {
+      evt.preventDefault();
+      return false;
+    });
+
+    photos[i].addEventListener('drop', function (evt) {
+      evt.target.insertBefore(draggedItem);
+      evt.preventDefault();
+    });
+  }
 })();
